c794f2e99de3a9d267dceaaf10d99b250f25292f
William Rodrigues <williamrodrigues2442@gmail.com>
2021-11-09 21:09:49 -0300

    Adiciona nodemon para desenvolvimento e coloca as rotas no plural

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/usuario', (_, res) => {
-  res.send(`Rota ativada com GET e recurso "usuario": valores de "usuario" devem ser retornados`);
+app.get('/usuarios', (_, res) => {
+  res.send(`Rota ativada com GET e recurso "usuarios": valores de "usuarios" devem ser retornados`);
 });
 
-app.get('/tarefa', (_, res) => {
-  res.send(`Rota ativada com GET e recurso "tarefa": valores de "tarefa" devem ser retornados`);
+app.get('/tarefas', (_, res) => {
+  res.send(`Rota ativada com GET e recurso "tarefas": valores de "tarefas" devem ser retornados`);
 });
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));

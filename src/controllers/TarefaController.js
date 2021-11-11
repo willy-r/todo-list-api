a83f2e33ea0e4596be7ce8c989852c2fde33a1b0
William Rodrigues <williamrodrigues2442@gmail.com>
2021-11-11 20:44:16 -0300

    Cria rotas POST para a criaçao de usuario e tarefa

--- src/controllers/TarefaController.js
+++ src/controllers/TarefaController.js
@@ -1,7 +1,11 @@
 function TarefaController(app) {
   app.get('/tarefas', (_, res) => {
-    res.send(`Rota ativada com GET e recurso "tarefas": valores de "tarefas" devem ser retornados`);
-  });  
+    res.send(`Rota ativada com GET e recurso "tarefa": valores de "tarefa" devem ser retornados`);
+  });
+
+  app.post('/tarefas/criar', (_, res) => {
+    res.send('Rota POST de "tarefa" ativada: tarefa adicionada ao banco de dados');
+  });
 }
 
 module.exports = TarefaController;
--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,6 +1,10 @@
 function UsuarioController(app) {
   app.get('/usuarios', (_, res) => {
-    res.send(`Rota ativada com GET e recurso "usuarios": valores de "usuarios" devem ser retornados`);
+    res.send(`Rota ativada com GET e recurso "usuario": valores de "usuario" devem ser retornados`);
+  });
+
+  app.post('/usuarios/criar', (_, res) => {
+    res.send('Rota POST de "usuario" ativada: usuário adicionado ao banco de dados');
   });
 }
 

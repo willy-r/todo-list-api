05b254b5c3cd623afde0a93354c7a899602ffa40
William Rodrigues <williamrodrigues2442@gmail.com>
2021-11-16 07:26:39 -0300

    Adiciona a lib cors

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const consign = require('consign');
+const cors = require('cors');
 
 function configuraExpress() {
   const app = express();
   
   // Middlewares.
-  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   // Rotas.
   consign().include('./src/controllers').into(app);
